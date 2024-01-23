23c80e6731c85c844264a4cf091ff85406f2a03f
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2024-01-23 22:18:51 +0700

    first commit

---
+++ 1-grow.js
@@ -0,0 +1,30 @@
+//fungsi biasa (mainstream)
+//megggunakan perulangan
+function grow(x){
+    let result = x[0];
+    for (let i = 1; i < x.length; i++){
+        result = result * x[i];
+    }
+    return result
+};
+
+//dengan menggunakan high order function "reduce"
+function grow(x){
+    const result = x.reduce((acc, curr) => {
+        return acc * curr;
+    },1); //angka 1 disini berfungsi untuk mengalikan bilangan pertama karena semua bilangan dikali 1 akan sama
+    return result
+}
+
+console.log(grow([1,2,3,4]))
+//disini acc(accumulator) = "sebagai nilai yg ditambahkan"
+// dan curr(current value) ="sebagai nilai saat ini"
+function grow(x){
+    const result = x.reduce((acc , curr) => acc*curr)
+    return result
+}
+console.log(grow([1,2,3,4]))
+
+const grow = (x) => x.reduce((acc,curr) => acc*curr)
+
+console.log(grow([1,2,3,4]))
