e90070821fadd68fbeb3a13bd700975e9c31b470
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2024-01-24 00:22:49 +0700

    tasiting_tu

--- tastingtu.js
+++ tastingtu.js
@@ -1,3 +1,10 @@
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
 //Batu gunting kertas
 const rps = (player1, player2) => {
     switch(true){
