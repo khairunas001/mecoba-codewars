e9904cd3372c03579e2e44a488b8b6aa2a121507
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2024-01-23 23:59:16 +0700

    Mantap_Manies

--- 1-grow.js
+++ 1-grow.js
@@ -1,4 +1,8 @@
 //day-1
+
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 //fungsi biasa (mainstream)
 //megggunakan perulangan
 function grow(x){
--- 2-reverseSeq.js
+++ 2-reverseSeq.js
@@ -1,4 +1,7 @@
 //2-reverseSeq
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
 //menggunakan perulangan terbalik
 const reverseSeq = n => {
     const result = [];
